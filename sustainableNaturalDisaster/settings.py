3820e655da5a51f8c6b97171722581148c26b306
jonny21099 <jcmartin@ualberta.ca>
2021-01-23 10:42:50 -0700

    Login and Logout complete

--- sustainableNaturalDisaster/settings.py
+++ sustainableNaturalDisaster/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'sustainableNaturalDisaster.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [str(BASE_DIR.joinpath('templates'))],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -118,3 +118,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# config/settings.py
+LOGIN_REDIRECT_URL = 'home'
+LOGOUT_REDIRECT_URL = 'home'

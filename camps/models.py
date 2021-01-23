1b4ea0c13ae45e92fa0a45deb079adfb27d05e13
XYSS <35908688+xiyuan1@users.noreply.github.com>
2021-01-23 12:05:46 -0700

    camps database

--- camps/models.py
+++ camps/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Camp(models.Model):
+    camp_name = models.CharField(max_length=50,primary_key=True)
+    water = models.IntegerField(max_length=1,help_text="Water storage capacity",editable=True)
+    food = models.IntegerField(max_length=1,help_text="Food storage capacity",editable=True)
+    blanket = models.IntegerField(max_length=1,help_text="Blanket storage capacity",editable=True)
+    drug = models.IntegerField(max_length=1,help_text="Drug storage capacity",editable=True)

07ce8e6d00e523cbf7fe2b2e6fca82470c540b51
d-pacheco <pacheco.daylen@gmail.com>
2021-01-23 14:48:28 -0700

    working on requests

--- camps/urls.py
+++ camps/urls.py
@@ -2,5 +2,6 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home')
+    path('', views.home, name='home'),
+    path('supply_requests/', views.supplyRequests, name='supply_requests'),
 ]

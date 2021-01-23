1d2c76a10c0c214322c022ebb75b3e696e037e3f
d-pacheco <pacheco.daylen@gmail.com>
2021-01-23 16:46:48 -0700

    tried fixing errors

--- camps/admin.py
+++ camps/admin.py
@@ -1,4 +1,9 @@
 from django.contrib import admin
 from .models import SupplyReqs
+from .models import Inventory
+#from .models import test_2
 
+
+admin.site.register(Inventory)
 admin.site.register(SupplyReqs)
+

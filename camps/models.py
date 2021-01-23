07dfdc6b353383f86f6e719de7e9956c3e94ff22
d-pacheco <pacheco.daylen@gmail.com>
2021-01-23 15:01:20 -0700

    fixed model error

--- camps/models.py
+++ camps/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 
-<<<<<<< HEAD
 class SupplyReqs(models.Model):
     camp_id = models.CharField(max_length=30)
     fullfilled = models.BooleanField(default=False)
@@ -11,11 +10,9 @@ class SupplyReqs(models.Model):
         return self.camp_id + ' | ' + self.item_id + ' | ' + str(self.item_amt) + ' | ' + str(self.fullfilled)
 
 
-=======
 # Create your models here.
 
 class Inventory(models.Model):
     camp_id = models.CharField(max_length=30)
     item_id = models.CharField(max_length=30)
     item_amt = models.PositiveIntegerField()
->>>>>>> e1db5518752df2116b64c684750942a0926ddd50

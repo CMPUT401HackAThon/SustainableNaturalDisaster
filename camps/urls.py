5f64ab062e8189e48086a4bd74cb5cd45b2e6953
d-pacheco <pacheco.daylen@gmail.com>
2021-01-23 18:04:02 -0700

    added requests page

--- camps/urls.py
+++ camps/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('supply_requests/', views.supplyRequests, name='supply_requests'),
+    path('fullfill_request/<request_id>', views.fullfill_request, name='fullfill_req'),
 ]
